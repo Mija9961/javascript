18e678cb5a203d0a099b4575bc2fdca44b39fa3a
Mija9961 <67261639+Mija9961@users.noreply.github.com>
2024-09-09 11:32:38 +0000

    Conversion of datatypes

---
+++ 03_conversionOperations.js
@@ -0,0 +1,36 @@
+let score = "33abc"
+// let score = null
+// let score = undefined
+
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+null => 0
+*/
+
+
+
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(typeof NaN)
+
+
+// let isLoggedIn = null
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+
+let someNumber = true
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
